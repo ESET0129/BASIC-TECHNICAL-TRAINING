a9fd4a267a6992770406a30f79f2103e5f60cb12
SHANTANU SWARAJ <shantanu.9993223184@gmail.com>
2025-09-16 10:45:20 +0530

    16th september DLMS ASSIGNEMNT 1

---
+++ DLMS/assignment 1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DLMSASSIGNEMNT1
+{
+    public class OBISCODE//obis code class
+    {
+        public byte A { get; set; }
+        public byte B { get; set; }
+        public byte C { get; set; }
+        public byte D { get; set; }
+        public byte E { get; set; }
+        public byte F { get; set; }
+
+        public OBISCODE(byte a, byte b, byte c, byte d, byte e, byte f) {
+            A = a; B = b; C = c; D = d; E = e; F = f;
+        
+        }
+
+        public byte[] ToByteArray()
+        {
+            return new byte[] { A, B, C, D, E, F };
+        }
+
+        public override string ToString()
+        {
+            return $"{A}-{B}:{C}.{D}.{E}.{F}";
+        }
+    }
+
+    // APDU builder
+    public class GetRequestApdu
+    {
+        public static byte[] CreateGetRequest(OBISCODE obis, ushort classId, byte attributeId)
+        {
+            List<byte> apdu = new List<byte>();
+
+            // GET-Request
+            apdu.Add(0xC0); // GET-Request
+            apdu.Add(0x01); // Get-Request-Normal
+            // Invoke ID and Priority
+            apdu.Add(0x01); // Ex: Invoke ID = 1, normal priority
+
+            // Class ID (2 bytes)
+            apdu.Add((byte)(classId >> 8)); // High byte
+            apdu.Add((byte)(classId & 0xFF)); // Low byte
+
+            // OBIS code
+            apdu.AddRange(obis.ToByteArray());
+
+            // Attribute ID
+            apdu.Add(attributeId);
+
+            // Access Selector (0 = no selective access)
+            apdu.Add(0x00);
+
+            return apdu.ToArray();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // OBIS code for Active Energy Import (1-0:1.8.0.255)
+            var obis = new OBISCODE(1, 0, 1, 8, 0, 255);
+
+            // Class ID for Register object is 3
+            ushort classId = 3;
+            byte attributeId = 2; // e.g., value attribute
+
+            byte[] apdu = GetRequestApdu.CreateGetRequest(obis, classId, attributeId);
+
+            Console.WriteLine("OBIS Code: " + obis);
+            Console.WriteLine("GET Request APDU: " + BitConverter.ToString(apdu));
+        }
+    }
+}
